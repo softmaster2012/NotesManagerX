9869ef39c3736d6ba9c00006043cf1ea876d3873
MP <gowthecore@gmail.com>
2017-03-14 20:20:29 +0200

    first form change

--- PersonalNotesManager/Form1.Designer.cs
+++ PersonalNotesManager/Form1.Designer.cs
@@ -39,20 +39,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 20);
+            this.label1.Location = new System.Drawing.Point(19, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Login";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 45);
+            this.label2.Location = new System.Drawing.Point(19, 45);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Password";
             // 
             // textBox1
             // 
@@ -74,8 +74,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -83,8 +84,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(129, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Censel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- PersonalNotesManager/Form1.cs
+++ PersonalNotesManager/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Security.Cryptography;
 
 namespace PersonalNotesManager
 {
     public partial class Form1 : Form
     {
+        string Path;
+        XDocument doc;
+        MD5 Codder;
+
         public Form1()
         {
             InitializeComponent();
+            Path = @"..\..\accounts.xml";
+            doc = XDocument.Load(Path);
+            Codder = MD5.Create();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Login = textBox1.Text;
+            string Passw = GetMd5Hash(textBox2.Text);
+            var res = from x in doc.Element("root").Elements("account")
+                      where x.Attribute("login").Value == Login && x.Attribute("passw").Value == Passw
+                      select x;
+
+
+        }
+
+        string GetMd5Hash(string input)
+        {
+            byte[] data =Codder.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
         }
     }
 }
